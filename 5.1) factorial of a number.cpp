968613da5e6933f8c8fb858fddb4addbd0eadbc0
Shaswat SInha <97851942+Lone0023@users.noreply.github.com>
2022-01-20 12:12:49 +0530

    Update 5.1) factorial of a number.cpp

--- 5.1) factorial of a number.cpp
+++ 5.1) factorial of a number.cpp
@@ -1,1 +1,23 @@
+//loops
+
+#include<iostream>
+using namespace std;
+
+int factorial(int n)
+{
+    int fac=1;
+    for(int i=1; i<=n; i++){
+        fac=fac*i;
+    }
+    return fac;
+}
+int main()
+{
+    int a;
+    cout<<"Enter  your number: ";
+    cin>>a;
+    cout<<"The factorial of "<<a<<" is: ";
+    cout<<factorial(a);
+    return 0;
+}
 
